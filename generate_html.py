85e0a61baaecd625e36da723f32c9e8f516d9885
thompson318 <s.thompson@ucl.ac.uk>
2023-09-12 15:52:45 +0000

    Deploying to gh_pages from @ thompson318/OSSSFinalReport@f9977bcb225869c941277041ed16424c201b3d0d 🚀

--- generate_html.py
+++
@@ -1,9 +0,0 @@
-import markdown
-
-with open('report.md', 'r') as f:
-    report_md = f.read()
-
-reportHTML = markdown.markdown(report_md)
-
-with open('report.html', 'w') as f:
-    f.write(reportHTML)
